cb50480e62ff5be16177f51a071fd45c769c3059
Kadam-Suraj <surajkadam381@gmail.com>
2024-11-30 22:19:08 +0530

    feat: introduce env variables for secure credentials

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: 'n2wxwdlq', // Sanity project ID
-  dataset: 'production', // Sanity dataset
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // Sanity project ID
+  dataset: import.meta.env.VITE_SANITY_DATASET, // Sanity dataset
   apiVersion: "2023-01-01", // Sanity API version
   useCdn: true, // `false` if you want to ensure fresh data
-  token: 'skRPWXPK6H6mcODzlE3u0vm9ketpH9wc1eMPL6HAn0PnIHruzYjpLoExaIJ4KF9a6zOHFAEJRioNFACz0odCkSWDNkhZrxlSVinTKaALHa6HJRbGgf8461jMpbLRv8msHyRy9Hs3Tw33M5j8gkxFF2VNueUCCzRwT003c2ArMYmSVEmkytPX',
+  token: import.meta.env.VITE_SANITY_API_TOKEN,
 });
 
 export default client;
